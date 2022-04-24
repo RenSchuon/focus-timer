d4086578a66cd5de28915b90aced6c780a5f6df9
Ren Schuon <64823398+RenSchuon@users.noreply.github.com>
2022-04-23 17:14:43 -0700

    WorkTime constructor test

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import unittest
+import WorkTime
  
 class TestTicTacToeMethod(unittest.TestCase):
-
+    def test_WorkTime_default_time(self):
+        ob = WorkTime.WorkTime()
+        self.assertEqual(ob.time, 1200)
 
 
 if __name__ == '__main__':
