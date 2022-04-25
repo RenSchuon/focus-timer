a51171189f6417df9086e7af804c53a3baac9b5a
ElizabethSchuon <lizzybetham64@gmail.com>
2022-04-25 11:44:26 -0700

    get_time test

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import unittest
 import WorkTime
  
-class TestTicTacToeMethod(unittest.TestCase):
+class FocusTimer(unittest.TestCase):
     def test_WorkTime_default_time(self):
         ob = WorkTime.WorkTime()
+        #time is default set to 1200 seconds or 20 min
         self.assertEqual(ob.time, 1200)
 
+    def test_WorkTime_get_time(self):
+        ob = WorkTime.WorkTime()
+        time = ob.get_time()
+        #default time is 20 min
+        self.assertEqual(time, 20)
 
 if __name__ == '__main__':
     unittest.main()
