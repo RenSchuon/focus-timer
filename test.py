027bd59a7b1f450019bdb4fd018c5c7d2eeaf03f
ElizabethSchuon <lizzybetham64@gmail.com>
2022-04-25 21:28:48 -0700

    get_sec tests

--- test.py
+++ test.py
@@ -7,11 +7,13 @@ class FocusTimer(unittest.TestCase):
         #time is default set to 1200 seconds or 20 min
         self.assertEqual(ob.time, 1200)
 
+
     def test_Timer_get_time(self):
         ob = Timer.Timer()
         time = ob.get_time()
         #default time is 20 min (1200 sec)
         self.assertEqual(time, 1200)
+
 
     def test_Timer_get_min_whole(self):
         ob = Timer.Timer()
@@ -19,13 +21,28 @@ class FocusTimer(unittest.TestCase):
         #default time is 20 min
         self.assertEqual(min, 20)
 
+
     def test_Timer_get_min_part(self):
         ob = Timer.Timer()
         ob.time = 1195
         #this should be 19 min 55 sec
-        time = ob.get_min()
-        self.assertEqual(time, 19)
+        min = ob.get_min()
+        self.assertEqual(min, 19)
 
+
+    def test_Timer_get_sec_none(self):
+        ob = Timer.Timer()
+        sec = ob.get_sec()
+        #default time is 20 min, no seconds should be here
+        self.assertEqual(sec, 0)
+
+
+    def test_Timer_get_min_some(self):
+        ob = Timer.Timer()
+        ob.time = 1195
+        #this should be 19 min 55 sec
+        sec = ob.get_sec()
+        self.assertEqual(sec, 55)
 
 
 if __name__ == '__main__':
