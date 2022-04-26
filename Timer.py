fe3f031fa618ddd08bcb1b6c245a0cc0e2ad572d
ElizabethSchuon <lizzybetham64@gmail.com>
2022-04-25 21:13:54 -0700

    made get_min

---
+++ Timer.py
@@ -0,0 +1,11 @@
+class Timer(object):
+      def __init__(self):
+        self.time = 1200
+
+      def get_time(self):
+        return self.time
+
+      def get_min(self):
+        return self.time/60
+
+      
--- WorkTime.py
+++
@@ -1,3 +0,0 @@
-class WorkTime(object):
-      def __init__(self):
-        self.time = 1200
