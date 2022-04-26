ed977b56ee1d701c51ef128e9e0d074a0f8cca8a
ElizabethSchuon <lizzybetham64@gmail.com>
2022-04-25 21:09:37 -0700

    updated some names and get_time function made

--- test.py
+++ test.py
@@ -1,17 +1,30 @@
 import unittest
-import WorkTime
+import Timer
  
 class FocusTimer(unittest.TestCase):
-    def test_WorkTime_default_time(self):
-        ob = WorkTime.WorkTime()
+    def test_Timer_default_time(self):
+        ob = Timer.Timer()
         #time is default set to 1200 seconds or 20 min
         self.assertEqual(ob.time, 1200)
 
-    def test_WorkTime_get_time(self):
-        ob = WorkTime.WorkTime()
+    def test_Timer_get_time(self):
+        ob = Timer.Timer()
         time = ob.get_time()
+        #default time is 20 min (1200 sec)
+        self.assertEqual(time, 1200)
+
+    def test_Timer_get_min_whole(self):
+        ob = Timer.Timer()
+        min = ob.get_min()
         #default time is 20 min
-        self.assertEqual(time, 20)
+        self.assertEqual(min, 20)
+
+    def test_Timer_get_min_part(self):
+        ob = Timer.Timer()
+        ob.time = 1195
+        #this should be 19 min 55 sec
+        time = ob.get_min()
+        self.assertEqual(time, 19)
 
 if __name__ == '__main__':
     unittest.main()
