1f4481487572a061cb4179b4b97cf431f95bd4c8
Ren Schuon <64823398+RenSchuon@users.noreply.github.com>
2022-04-27 17:00:31 -0700

    change off selection test

--- test.py
+++ test.py
@@ -167,13 +167,13 @@ class FocusTimer(unittest.TestCase):
         self.assertEqual(ob.option(), 2)
 
 
-    #@patch("builtins.input", side_effect=[1, 0])
-    #def test_Menu_option_change_off(self, mock_input):
+    @patch("builtins.input", side_effect=[1, 0])
+    def test_Menu_option_change_off(self, mock_input):
         #user should be prompted to put in selection in function before this and then option called
         #this option will open menu to change break time
-        #ob = Menu.Menu()
-        #ob.select = 3
-        #self.assertEqual(ob.option(), 3)
+        ob = Menu.Menu()
+        ob.select = 3
+        self.assertEqual(ob.option(), 3)
 
 
 
